39694baff0a19c3ac67b56b5d9565e9f2a14207d
Daniel Berezovski <danieldaniber@gmail.com>
2018-11-09 14:56:36 -0500

    added logout and register backend functionality

--- Web/laze/laze/settings.py
+++ Web/laze/laze/settings.py
@@ -114,6 +114,7 @@ USE_L10N = True
 USE_TZ = True
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
--- Web/laze/users/urls.py
+++ Web/laze/users/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from .views import register
 
 urlpatterns = [
 	url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html')),
+	url(r'^logout/$', auth_views.LogoutView.as_view()),
+	url(r'^register/$', register),
 ]
