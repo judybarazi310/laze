8cc5558277cf263a87c60cd677ecde36d046c0e6
Alejandro <alrolja@gmail.com>
2018-11-12 12:47:38 -0500

    Added basic pin placement

--- Web/laze/static/users/js/mapview.js
+++ Web/laze/static/users/js/mapview.js
@@ -3,59 +3,29 @@
 // The maximum width of the info window is set to 200 pixels.
 
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var wlu = {lat: 43.4735, lng: -80.5273};
     var tims = {lat: 43.4732, lng: -80.5255};
     var subway = {lat: 43.4727, lng: -80.5265};
-    var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 17,
-    center: tims
-    });
 
-    var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-        '<div id="bodyContent">'+
-        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
-        '</div>'+
-        '</div>';
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: wlu,
+        streetViewControl: false,
+        fullscreenControl: false
+    });
     
     var timsString = '<h1>Tims</h1>';
     var subwayString = '<h1>Subway Bricker</h1>';
-
-
-    var infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    maxWidth: 200
-    });
 
     var timswindow = new google.maps.InfoWindow({
     content: timsString,
     maxWidth: 200
     });
-
     var subwaywindow = new google.maps.InfoWindow({
     content: subwayString,
     maxWidth: 200
     });
 
-    var marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-    title: 'Uluru (Ayers Rock)'
-    });
     var timsmarker = new google.maps.Marker({
     position: tims,
     map: map,
@@ -66,13 +36,36 @@ function initMap() {
     map: map,
     title: 'Subway Bricker'
     });
-    marker.addListener('click', function() {
-    infowindow.open(map, marker);
-    });
+
     timsmarker.addListener('click', function(){
     timswindow.open(map, timsmarker);
     })
     subwaymarker.addListener('click', function(){
     subwaywindow.open(map, subwaymarker);
+    })
+
+    clickListener(map);
+}
+
+function clickListener(map) {
+    map.addListener('click', function(event) {
+        var userin = prompt("What is happening here?");
+        if (userin != null && userin != ''){
+            var newCoordinates = {lat: event.latLng.lat(), lng: event.latLng.lng()};
+            var newMarkerString = '<h1>' + userin + '</h1>';
+            var newMarker = new google.maps.Marker({
+                position: newCoordinates,
+                map: map,
+                title: 'New marker'
+            });
+            var newMarkerWindow = new google.maps.InfoWindow({
+                content: newMarkerString,
+                maxWidth: 200
+            });
+            newMarker.addListener('click', function(){
+                newMarkerWindow.open(map, newMarker);
+            })
+        }
+
     })
 }
